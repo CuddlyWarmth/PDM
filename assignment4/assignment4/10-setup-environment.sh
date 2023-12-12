2addfbbe9f6f4fa7b17e316c7aab3ab07d7de9f2
CuddlyWarmth <116100124+CuddlyWarmth@users.noreply.github.com>
2023-12-12 12:38:41 +0100

    pushed

---
+++ assignment4/assignment4/10-setup-environment.sh
@@ -0,0 +1,8 @@
+wget -O miniforge.sh https://github.com/conda-forge/miniforge/releases/download/23.3.1-0/Miniforge3-23.3.1-0-$(uname)-$(uname -m).sh
+bash miniforge.sh -b -p $PWD/miniforge3
+
+. $PWD/miniforge3/bin/activate
+printf "\n-----------\nminiforge3 is setup. The following should now point to a conda path:\n"
+which conda
+./00-setup.sh
+./02-start-notebook.sh
